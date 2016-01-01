f913b9cf07fa29572054118abdf9d2a04ef93904
Keith Smiley <keithbsmiley@gmail.com>
2015-12-31 16:49:52 -0800

    Allow github codeblocks to scroll horizontally

--- github-dark.sh
+++ github-dark.sh
@@ -5,6 +5,12 @@ url=https://raw.githubusercontent.com/StylishThemes/GitHub-Dark/master/github-da
 
 curl "$url" -o "$file"
 
+# This is kind of a hack, allow code blocks to scroll
+sed -i "" "s=.*nowrap=/**=" "$file"
+
+# Set my background image of choice
 sed -E -i "" "s/\/\*\[\[bg-choice\]\].*$/none;/" "$file"
+
+# Remove the moz annotations
 sed -i "" "/moz-document/d" "$file"
 sed -i "" "$ d" "$file"
